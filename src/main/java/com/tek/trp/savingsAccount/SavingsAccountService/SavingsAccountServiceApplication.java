8c4e0dc51a7da0b1fcb44eaccf24b126d5ec82bf
Burli <laburli@teksystems.com>
2020-02-04 13:09:10 +0530

    initial commin

---
+++ src/main/java/com/tek/trp/savingsAccount/SavingsAccountService/SavingsAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tek.trp.savingsAccount.SavingsAccountService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tek/trp/savingsAccount/SavingsAccountService/SavingsAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tek.trp.savingsAccount.SavingsAccountService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
